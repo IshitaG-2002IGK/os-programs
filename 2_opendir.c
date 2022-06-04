a6f90b6e5940b5b4c1a930d9e968abfda0532ade
Ishita <ishitagops12@gmail.com>
2022-06-04 22:06:55 +0530

    opendir and readdir

---
+++ 2_opendir.c
@@ -0,0 +1,12 @@
+#include<sys/types.h>
+#include<dirent.h>
+#include<stdio.h>
+main(int c,char* arg[])
+{
+    DIR *d;
+    struct dirent *r;
+    d=opendir(arg[1]);
+    printf("\n\t Name of the Files \n");
+    while((r=readdir(d)) != NULL)
+                printf("\t %s \n",r->d_name);
+}
