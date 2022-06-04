8979f20388d333856e69007c005f49a2af8291ab
Ishita <ishitagops12@gmail.com>
2022-06-04 22:34:57 +0530

    greater.sh

---
+++ greater.sh
@@ -0,0 +1,31 @@
+echo "Enter A" 
+read a 
+echo "Enter B" 
+read b 
+echo "Enter C" 
+read c 
+if [ $a -gt $b -a $a -gt $c ] 
+then 
+echo "A is greater" 
+elif [ $b -gt $a -a $b -gt $c ] 
+then 
+echo "B is greater" 
+else 
+echo "C is greater" 
+fi 
+
+
+
+
+
+
+
+
+
+
+
+
+:wq!
+
+
+
